72b016bc73f3d2985e7ee0a546549d896ea86947
Nithin Dampetla <nithinchakravarthyd@gmail.com>
2018-01-24 19:09:50 -0600

    Initial Message

---
+++ smackapp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  smackapp
+//
+//  Created by Nithin Chakravarthy Dampetla on 1/24/18.
+//  Copyright © 2018 Nithin Chakravarthy Dampetla. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
